540ac2e719fc77a0f13f9bc9151039341b064149
Farhad <nakhaie.farhad@gmail.com>
2024-01-13 18:02:51 +0330

    filtering objects

--- django_mosh_tutorial/storefront/playground/views.py
+++ django_mosh_tutorial/storefront/playground/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from store.models import OrderItem
 
 def say_hello(request):
     return HttpResponse('Hello World')
 
 def say_hello_html(request):
-    return render(request, 'hello.html', {'name': 'Farhad'})
+    query_set = OrderItem.objects.filter(product__collection__id=3)
+
+    return render(request, 'hello.html', {'name': 'Farhad', 'order_items': list(query_set)})
