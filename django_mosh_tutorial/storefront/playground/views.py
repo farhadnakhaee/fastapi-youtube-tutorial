13a56d6fcda868caf4ded5996ce1159d363fd22e
Farhad <nakhaie.farhad@gmail.com>
2024-01-18 20:14:41 +0330

    setting up admin site

--- django_mosh_tutorial/storefront/playground/views.py
+++ django_mosh_tutorial/storefront/playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import transaction
 from django.db.models import Q, F, Func, Value, ExpressionWrapper, DecimalField
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.contrib.contenttypes.models import ContentType
@@ -120,3 +121,17 @@ def say_hello(request):
 
 
 # transaction
+def say_hello_html(request):
+    # with transaction.atomic():
+    #     order = Order()
+    #     order.customer_id = 1
+    #     order.save()
+
+    #     item = OrderItem()
+    #     item.order = order
+    #     item.product_id = 1
+    #     item.quantity = -1
+    #     item.unit_price = 10
+    #     item.save()
+
+        return render(request, 'hello.html', {'name': 'Farhad'})
