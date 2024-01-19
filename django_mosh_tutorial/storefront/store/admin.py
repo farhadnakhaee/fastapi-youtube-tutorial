3c3e768e8c507eb2bad893a328a89b201430fa38
Farhad <nakhaie.farhad@gmail.com>
2024-01-19 16:59:14 +0330

    link to other page

--- django_mosh_tutorial/storefront/store/admin.py
+++ django_mosh_tutorial/storefront/store/admin.py
@@ -1,7 +1,30 @@
+from typing import Any
+from django.db.models import Count
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = \
+            reverse('admin:store_product_changelist') \
+            + '?' \
+            + urlencode({'collection__id': str(collection.id)})
+            
+        return format_html('<a href={}>{}</a>', url, collection.products_count)
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -21,10 +44,23 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'order_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+    @admin.display(ordering='order_count')
+    def order_count(self, customer):
+        url = \
+            reverse('admin:store_order_changelist') \
+            + '?' \
+            + urlencode({'customer_id': str(customer.id)})
+        return format_html('<a href={}>{}</a>', url, customer.order_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            order_count= Count('order')
+        )
 
 
 @admin.register(models.Order)
