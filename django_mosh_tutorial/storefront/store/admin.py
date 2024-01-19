a86b39f55778dfcb8c3bea43196a44aba4b47422
Farhad <nakhaie.farhad@gmail.com>
2024-01-19 13:03:22 +0330

    selcting related objects

--- django_mosh_tutorial/storefront/store/admin.py
+++ django_mosh_tutorial/storefront/store/admin.py
@@ -5,9 +5,13 @@ admin.site.register(models.Collection)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, Product):
+        return Product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, Product):
@@ -21,3 +25,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'payment_status', 'placed_at']
+    list_per_page = 10
+        
