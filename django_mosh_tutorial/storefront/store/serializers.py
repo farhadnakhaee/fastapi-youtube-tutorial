eebfbd7a08988e07fc17849d8691b31411f71115
Farhad <nakhaie.farhad@gmail.com>
2024-01-29 22:43:44 +0330

    create, get, put, delete objects

--- django_mosh_tutorial/storefront/store/serializers.py
+++ django_mosh_tutorial/storefront/store/serializers.py
@@ -1,19 +1,20 @@
-from .models import Collection
+from .models import Collection, Product
 from decimal import Decimal
 from rest_framework import serializers
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title','inventory', 'slug', 'description', 'unit_price', 'price_with_tax', 'collection']
     
+
     # collection = serializers.PrimaryKeyRelatedField(
     #     queryset = Collection.objects.all()
     # )
@@ -22,10 +23,12 @@ class ProductSerializer(serializers.Serializer):
 
     # collection = CollectionSerializer()
 
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name='collection-detail',
-    )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name='collection-detail',
+    # )
+        
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
-    def calculate_tax(self, product):
+    def calculate_tax(self, product:Product):
         return product.unit_price * Decimal(1.1)
