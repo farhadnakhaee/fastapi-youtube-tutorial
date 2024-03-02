69ae3f599b7785569f68d9061258d5ba363767f8
Farhad <nakhaie.farhad@gmail.com>
2024-03-03 00:02:21 +0330

    django-mosh: add pagination, filter and search to viewset

--- django_mosh_tutorial/storefront/store/views.py
+++ django_mosh_tutorial/storefront/store/views.py
@@ -1,17 +1,11 @@
-import select
-from typing import Any
-from django.apps import AppConfig
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django.db.models import Count
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework import status
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .filters import ProductFilter
+from .pagination import DefaultPagination
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
@@ -19,9 +13,12 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
-    
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
+
     def get_serializer_context(self):
         return {'request': self.request}
     
